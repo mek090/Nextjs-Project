909a05b1f03e15520f9335c034d3ad965488a25d
Rittipon 024 <49582945+mek090@users.noreply.github.com>
2025-05-21 18:05:26 +0700

    delete col-3 on homepage

--- components/home/LocationContainer.tsx
+++ components/home/LocationContainer.tsx
@@ -42,7 +42,7 @@ const LocationContainer = () => {
             {isLoading ? (
                 <section className="py-10">
                     <div className="container mx-auto px-4">
-                        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+                        <div className="grid grid-cols-1 sm:grid-cols-2  xl:grid-cols-4 gap-6">
                             {[...Array(4)].map((_, index) => (
                                 <LoadingCard key={index} />
                             ))}
--- components/home/LocationList.tsx
+++ components/home/LocationList.tsx
@@ -9,7 +9,7 @@ const LocationList = ({ Locations }: { Locations: LocationCardProps[] }) => {
     return (
         <section className="py-10">
             <div className="container mx-auto px-4">
-                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+                <div className="grid grid-cols-1 sm:grid-cols-2  xl:grid-cols-4 gap-6">
                     {Locations.slice(0, 4).map((Location) => (
                         <div 
                             key={Location.id} 
